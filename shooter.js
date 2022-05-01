8454c6ff7196242f71c0bd70c633550b0668e9f0
Rafael <rcarneiro782@gmail.com>
2022-04-30 21:04:46 -0300

    part 3 and 4

--- shooter.js
+++ shooter.js
@@ -25,3 +25,39 @@ function moveUp(){
         yourShip.style.top = `${position}px`
     }
 }
+
+
+function moveDown(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "510px"){
+        return
+    }else{
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+
+function firelaser(){
+let laser = createrLaserElement();
+playArea.appendChild(laser);
+moveLaser();
+}
+
+function createrLaserElement(){
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = 'img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left =`${xPosition}px`;
+    newLaser.style.top =`${yPosition - 10}px`;
+    return newLaser;
+}
+
+function  (){
+
+}
+
+window.addEventListener('keydown', flyShip);
